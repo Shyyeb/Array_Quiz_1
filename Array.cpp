dee1332e3ff5444dfa396f32abbe9395d964fc62
Shyyeb Santillan <shyyeb.santillan@cit.edu>
2026-03-01 15:59:38 +0800

    My quiz answers

--- Array.cpp
+++ Array.cpp
@@ -24,17 +24,49 @@ bool Array::Add(int value) {
 
 // Insert value at specified index, shifting elements to the right
 bool Array::InsertAt(int index, int value) {
-    return false;
+    // 1. Check if index is valid and if there is space
+    if (index < 0 || index > size_ || size_ == capacity_) {
+        return false;
+    }
+
+    // 2. Shift elements to the right to make a hole
+    for (int i = size_; i > index; i--) {
+        data_[i] = data_[i - 1];
+    }
+
+    // 3. Place the new value and grow the size
+    data_[index] = value;
+    size_++;
+    return true;
 }
 
 // Delete element at index, shifting elements left
 bool Array::DeleteAt(int index) {
-    return false;
+    // 1. Check if index is valid
+    if (index < 0 || index >= size_) {
+        return false;
+    }
+
+    // 2. Shift elements to the left to fill the gap
+    for (int i = index; i < size_ - 1; i++) {
+        data_[i] = data_[i + 1];
+    }
+
+    // 3. Shrink the size
+    size_--;
+    return true;
 }
 
 // Update element at index
 bool Array::UpdateAt(int index, int new_value) {
-    return false;
+    // 1. Check if index exists
+    if (index < 0 || index >= size_) {
+        return false;
+    }
+
+    // 2. Replace the value
+    data_[index] = new_value;
+    return true;
 }
 
 // Print all elements
